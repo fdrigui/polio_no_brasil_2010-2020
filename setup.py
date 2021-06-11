c00dab420c6b9d45b1ed8c53a8333e16aeb30ac7
fdrigui <filipi.rigui@outlook.com>
2021-06-10 23:57:39 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Projeto de exploração dos dados do Sistema Unico de Saude do Brasil, com o foco nos casos de Poliomelite e a relação com o programa de vacinação.',
+    author='Filipi Destro Rigui',
+    license='MIT',
+)
